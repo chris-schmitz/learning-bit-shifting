3e2732729c3fa156166f9fe1591edefd2336dace
chris-schmitz <schmitz.chris@gmail.com>
2019-02-08 13:13:09 -0600

    Initial Commit

---
+++ BitShiftExample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BitShiftExample
+//
+//  Created by Christopher Schmitz on 2/8/19.
+//  Copyright © 2019 Christopher Schmitz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
