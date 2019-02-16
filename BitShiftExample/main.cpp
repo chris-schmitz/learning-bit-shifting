bed4035a2f5654c214d4cae25700bc3f6bf0e8fe
chris-schmitz <schmitz.chris@gmail.com>
2019-02-15 18:26:51 -0600

    Working through using bit shifting to push int values onto a long

--- BitShiftExample/main.cpp
+++ BitShiftExample/main.cpp
@@ -1,15 +1,36 @@
-//
-//  main.cpp
-//  BitShiftExample
-//
-//  Created by Christopher Schmitz on 2/8/19.
-//  Copyright © 2019 Christopher Schmitz. All rights reserved.
-//
-
 #include <iostream>
 
+enum printType {
+    DECIMAL,
+    HEXADECIMAL,
+};
+
+// declarations
+void printNumber (long value, printType type);
+long shift(long value);
+
+int idArray[] = {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, 0x11};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    long compiled = 0;
+
+    for (uint8_t i = 0; i < 8; i++){
+        compiled = shift(compiled);
+        compiled +=(long) idArray[i];
+    }
+    
+    printNumber(compiled, HEXADECIMAL);
+    
+}
+
+long shift(long value) {
+    return value << 8;
+}
+
+void printNumber (long value, printType type) {
+    if (type == DECIMAL) {
+        std::cout << value << std::endl;
+    } else if (type == HEXADECIMAL) {
+        std::cout << std::hex << value << std::endl;
+    }
 }
